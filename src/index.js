f813ff9b9aaf14fa2f108feda9891f83ffda1669
RAGHURAJ SINGH RAJPOOT <114226990+Raghuraj361@users.noreply.github.com>
2023-01-08 23:24:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 
 
+class API{
+  constructor(url)
+  this.url = url
+  this.method = "GET"
+  this.#secure = url.startWith("https");
+}
+isSecure() {
+    return this.#secure
+}
+updateURL(url) {
+  this.url = url
+}
+let firstAPI = new API('http://api. com/api/hello')
+let secondAPI = new API('https://api. com/api/hello')
+    
 module.exports = { API }
